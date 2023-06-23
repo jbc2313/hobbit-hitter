5e5c578bc493abe205087926b3abaf18b7b1b360
jb <101614021+jbc2313@users.noreply.github.com>
2023-06-23 03:50:06 -0500

    Added code
    Added function for left side of hobbit body

--- src/hobbit_hitter/core.clj
+++ src/hobbit_hitter/core.clj
@@ -1,7 +1,28 @@
 (ns hobbit-hitter.core
   (:gen-class))
 
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thight" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "lfet-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "This will run the main code"
   [& args]
-  (println "Hello, World!"))
+  (println "Lets beat down some hobbits!"))
